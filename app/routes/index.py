3b02dab21d72047f8751f47eb8a4fafd7587766e
qmShen <joyshen06@gmail.com>
2017-09-06 14:03:07 +0800

    save

--- app/routes/index.py
+++ app/routes/index.py
@@ -14,9 +14,9 @@ station_ids = ['admiralty']
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/test')
-def getStationConfig():
-    return json.dumps("test")
+# @app.route('/test')
+# def getStationConfig():
+#     return json.dumps("test")
 
 @app.route('/getStationMap',  methods = ['POST'])
 def get_map():
